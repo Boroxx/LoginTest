24fea929e98d08e34375b7fd96788ac5032e4ce7
Boroxx <tenelsen.boris@web.de>
2020-01-08 23:55:06 +0100

    Form-Login with Spring Security

--- src/main/java/com/boristenelsen/LoginFormTest/LoginFormTestApplication.java
+++ src/main/java/com/boristenelsen/LoginFormTest/LoginFormTestApplication.java
@@ -2,6 +2,13 @@ package com.boristenelsen.LoginFormTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+*
+* Wichtig merke:
+* Wenn du eine eigene Login-Form mit Thymeleaf baust dann achte darauf dass du die verfickten attribute vom user und password Feld auch genau so nennst.
+*
+*
+* */
 
 @SpringBootApplication
 public class LoginFormTestApplication {
